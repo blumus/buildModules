c34946a3d0ff9cdc724638d44eeea14cb5e8a74b
Baruch Leiman <leiman.baruch@gmail.com>
2025-04-23 10:16:25 +0000

    cyclic catch

--- run_build.py
+++ run_build.py
@@ -3,11 +3,16 @@ import json
 def create_build_list(modules: object):
     build_list=[]
     processed = set()
+    started_processing = set()
     
     def process_build(module_id: str):
         # if already processed ignore
         if module_id in processed:
             return
+        # if module has already start process - then further start process on build is circular
+        if module_id in started_processing:
+            raise Exception("Your graph contains a cyclic module definition.")
+        started_processing.add(module_id)
         #process module dependcies
         for d in modules[module_id]["dependencies"]:
             process_build(d)
@@ -25,4 +30,7 @@ if __name__ == "__main__":
     with open("modules.json","r") as f:
         modules = json.load(f)
 
-    print(",".join(create_build_list(modules)))
+    try:
+        print(",".join(create_build_list(modules)))
+    except Exception as e:
+        print(f"{e}")
